f7dead64b8c10b19359599bdf4edda02e7f83a24
Andrew Martin <andrew.martin@wustl.edu>
2024-02-12 13:55:29 -0600

    mount and launch script for HUMANN and METAPHLAN

--- mountAndLaunchHumann.sh
+++ mountAndLaunchHumann.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LSF_DOCKER_VOLUMES='/storage1/fs1/rnewberry/Active:/storage1/fs1/rnewberry/Active/'
+export LSF_DOCKER_VOLUMES='/storage1/fs1/rnewberry/Active:/storage1/fs1/rnewberry/Active/ /scratch1/fs1/rnewberry/Andrew:/scratch1/fs1/rnewberry/Andrew'
 export LSF_DOCKER_NETWORK=host
 export LSF_DOCKER_IPC=host
 
