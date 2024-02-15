41e4c99ff1608e79f0016509f75df57cefe992e3
Andrew Martin <andrew.martin@wustl.edu>
2024-02-15 12:15:47 -0600

    update resources requested

--- runMetaphlan.sh
+++ runMetaphlan.sh
@@ -7,4 +7,4 @@
 infile_fwd=/storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/PE_Test_files/${1}
 infile_rev=/storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/PE_Test_files/${2}
 
-bsub -n 8 -R 'rusage[mem=32GB]' -q general -a 'docker(biobakery/humann)' metaphlan storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/PE_Test_files/trimmed_unpairedERR101899_1.fastq --input_type fastq --nproc 8 -o scratch1/fs1/rnewberry/Andrew/profiled_metagenome.txt --bowtie2db scratch1/fs1/rnewberry/Andrew/mpa_vJun23_CHOCOPhlAnSGB_202307 --index mpa_vJun23_CHOCOPhlAnSGB_202307
+bsub -n 16 -R 'rusage[mem=32GB]' -q general -a 'docker(biobakery/humann)' metaphlan storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/PE_Test_files/trimmed_unpairedERR101899_1.fastq --input_type fastq --nproc 8 -o scratch1/fs1/rnewberry/Andrew/profiled_metagenome.txt --bowtie2db scratch1/fs1/rnewberry/Andrew/mpa_vJun23_CHOCOPhlAnSGB_202307 --index mpa_vJun23_CHOCOPhlAnSGB_202307
