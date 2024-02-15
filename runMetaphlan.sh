a043adb97f94494add6bbb56c64418096fe79451
Andrew Martin <andrew.martin@wustl.edu>
2024-02-15 17:05:31 -0600

    resource update

--- runMetaphlan.sh
+++ runMetaphlan.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#unzip fastq file
-
-
 #gets directory
 infile_fwd=/storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/PE_Test_files/${1}
 infile_rev=/storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/PE_Test_files/${2}
