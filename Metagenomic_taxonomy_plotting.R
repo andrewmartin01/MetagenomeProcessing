ee041446d3bffca1b82a6356ddd6d7f18b6e6272
Andrew Martin <andrew.martin@wustl.edu>
2024-03-05 21:21:45 -0600

    R plotting of metagenomic sequencing

--- Metagenomic_taxonomy_plotting.R
+++ Metagenomic_taxonomy_plotting.R
@@ -1,13 +1,13 @@
-##Author
-##Date
-##Purpose
+##Author: Andrew Martin
+##Date: 3/5/2024
+##Purpose: Make nice plots of data from MetaPhlan
 
 #packages------------------------------------------------------------------------
 install.packages(c("gplots", "viridis"))
 library(gplots)
 library(viridis)
 
-#import data---------------------------------------------------------------------
+#Import data---------------------------------------------------------------------
 
 #Plot Heatmap--------------------------------------------------------------------
 m <- as.matrix(Averaged_pooled_abundance_table[, -1])
@@ -15,8 +15,25 @@ rownames(m) <- Averaged_pooled_abundance_table$Species
 #plot heatmap without strain dendogram
 heatmap(m,Rowv = NA,col=magma(300))
 
+#Plot stacked barplot------------------------------------------------------------
 
+#remove all rows (species) that make up <3% of any sample
+new_df <- Averaged_pooled_abundance_table[rowSums(Averaged_pooled_abundance_table[, -1] > 3) > 0, ]
 
+#relative abundance
+
+#adjust margins
+par(mar=c(4, 3, 3, 12), xpd=TRUE)
+par(las=2)
+barplot(m, main = "Relative Abundance",col = rainbow(9))
+legend("topright", inset = c(-0.67, 0), title = "Species",legend= c(as.vector(new_df$Species))
+       , horiz=FALSE,fill=rainbow(9),cex=0.8)
+
+#axis(1, at = 1:ncol(m), labels = FALSE, tick = FALSE)
+#text(1:ncol(m), par("usr")[3] - 0.5, labels = new_df$Species, srt = 90, adj = 1, xpd = TRUE, cex = 0.8)
+
+#total abundance
+barplot()
 
 #Plot alpha and beta diversity---------------------------------------------------
 
