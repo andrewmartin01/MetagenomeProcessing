c9c04fc00fcdef2c0f5eb52b2341f09babe53bd3
Andrew Martin <andrew.martin@wustl.edu>
2024-04-02 11:26:22 -0500

    initial humanN trial

--- runHumanN.sh
+++ runHumanN.sh
@@ -5,30 +5,21 @@
 #purpose:
 
 export LSF_DOCKER_VOLUMES='/storage1/fs1/rnewberry/Active:/storage1/fs1/rnewberry/Active/ /scratch1/fs1/rnewberry/Andrew:/scratch1/fs1/rnewberry/Andrew'
-directory_path=storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/Seq_Files_Real/
+directory_path=scratch1/fs1/rnewberry/Andrew/trimmomatic_output/
 
 #make a job group
 bgadd -L 50 /andrew.martin/HumanN
 
 # Use find to get a list of files in the directory
-files=$(find "$directory_path" -type f -name "*_001.fastq.gz")
-
-
+files=$(find "$directory_path" -type f -name "*.fastq.gz")
 
 # Iterate through each file
 for file1 in $files; do
-    # Extract the base name
-    base_name=$(basename "$file1" "1_001.fastq.gz")
 
-    # Form the pair file name
-    file2="$directory_path$base_name"2_001.fastq.gz
-
-    # Check if the pair file exists
-    if [ -e "$file2" ]; then
-        #makes a directory for storing the output
-        mkdir scratch1/fs1/rnewberry/Andrew/trimmomatic_output/${base_name}/
+    #makes a directory for storing the output
+    #mkdir scratch1/fs1/rnewberry/Andrew/trimmomatic_output/${base_name}/
           
-        #runs humanN on all samples for functional profiling
-        bsub -g /andrew.martin/HumanN -n 20 -R 'rusage[mem=32GB]' -q general -a 'docker(biobakery/humann)' --nucleotide-database /storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/HumanN_database/HumanN_Chocophlan --protein-database /storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/HumanN_database/UniRef90_db
-    fi
+    #runs humanN on all samples for functional profiling
+    bsub -g /andrew.martin/HumanN -n 20 -R 'rusage[mem=32GB]' -q general -a 'docker(biobakery/humann)' --input $directory_path/${file1} --output storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/HumanN_output/ --nucleotide-database /storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/HumanN_database/HumanN_Chocophlan --protein-database /storage1/fs1/rnewberry/Active/Andrew_metagenome_seq/HumanN_database/UniRef90_db
+    
 done
